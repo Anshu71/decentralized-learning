00753548c89b058777eec218e067eb9330331465
Harsh Jha <hjha0695@gmail.com>
2023-09-16 05:16:26 +0530

    more code reverted

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import Signup from "./components/Signup";
+import Signup from "./components/Signup";
 import Webdev from "./pages/Webdev/Webdev";
 import WebHtml from "./pages/Webdev/WebHtml";
 import Landing from "./pages/landing";
 // import Join from "./pages/join";
 // import RoomPage from "./pages/room";
-// import Login from "./components/Login";
+import Login from "./components/Login";
 
 
 function App() {
@@ -18,8 +18,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Landing />} />
-          {/* <Route path="/signup" element={<Signup />} /> */}
-          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/webdev" element={<Webdev />} />
           <Route path="/webhtml" element={<WebHtml />} />
           {/* <Route path="/join" element={<Join />} /> */}
